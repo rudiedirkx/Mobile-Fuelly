402c4e4f001351475650b8660a1807e4dde4e093
Rudie Dirkx <github@hotblocks.nl>
2016-02-05 19:59:35 +0100

    vehicle page+

--- vehicle.php
+++ vehicle.php
@@ -3,8 +3,34 @@
 require 'inc.bootstrap.php';
 
 $vehicle = $client->getVehicle($_GET['id']);
+$fuelups = $client->getFuelUpsWithIds($vehicle);
 
 include 'tpl.header.php';
 
 ?>
-<pre><? print_r($vehicle) ?></pre>
+<h1><?= html($vehicle->name) ?></h1>
+
+<h2>Fuel-ups</h2>
+
+<ul>
+	<? foreach ($fuelups as $fuelup): ?>
+		<li>
+			<h3><?= $fuelup->date->format('j M Y') ?></h3>
+			<p>Eff.: <?= number_format($fuelup->mileage->amount, 2) ?></p>
+		</li>
+	<? endforeach ?>
+</ul>
+
+<details>
+	<summary>Vehicle</summary>
+	<pre><? print_r($vehicle) ?></pre>
+</details>
+
+<details>
+	<summary>Fuel-ups</summary>
+	<pre><? print_r($fuelups) ?></pre>
+</details>
+
+<?php
+
+include 'tpl.footer.php';
