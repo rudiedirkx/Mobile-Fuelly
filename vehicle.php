e5a085208ce79890853c44c48ef35bd403d4067b
Rudie Dirkx <github@hotblocks.nl>
2016-02-08 00:16:59 +0100

    +fuel up form

--- vehicle.php
+++ vehicle.php
@@ -3,12 +3,27 @@
 require 'inc.bootstrap.php';
 
 $vehicle = $client->getVehicle($_GET['id']);
-$fuelups = $client->getFuelUpsWithIds($vehicle);
+
+if ( isset($_POST['distance'], $_POST['amount'], $_POST['date']) ) {
+	print_r($_POST);
+	exit;
+}
 
 include 'tpl.header.php';
 
+$fuelups = $client->getFuelUpsWithIds($vehicle);
+
 ?>
 <h1><?= html($vehicle->name) ?></h1>
+
+<h2>Fuel up</h2>
+
+<form method="post" action="?id=<?= (int) @$_GET['id'] ?>">
+	<p>Distance: <input type="number" name="distance" /> km</p>
+	<p>Fuel amount: <input type="number" name="amount" /> l</p>
+	<p>Date: <input type="date" name="date" value="<?= date('Y-m-d') ?>" /></p>
+	<p><button>Save</button></p>
+</form>
 
 <h2>Fuel-ups</h2>
 
@@ -21,6 +36,7 @@ include 'tpl.header.php';
 	<? endforeach ?>
 </ul>
 
+<? /*
 <details>
 	<summary>Vehicle</summary>
 	<pre><? print_r($vehicle) ?></pre>
@@ -30,6 +46,7 @@ include 'tpl.header.php';
 	<summary>Fuel-ups</summary>
 	<pre><? print_r($fuelups) ?></pre>
 </details>
+*/ ?>
 
 <?php
 
