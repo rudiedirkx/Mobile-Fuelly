6cfb72438c32999c870ff0347e3df11c877a962a
Rudie Dirkx <github@hotblocks.nl>
2016-04-05 22:51:42 +0200

    +fuelup mileage bars

--- vehicle.php
+++ vehicle.php
@@ -11,9 +11,48 @@ if ( isset($_POST['distance'], $_POST['amount'], $_POST['date']) ) {
 
 include 'tpl.header.php';
 
-$fuelups = $client->getFuelUpsWithIds($vehicle);
+$fuelups = $client->getFuelUpsWithIds($vehicle, 60);
+$max = array_reduce($fuelups, function($max, $fuelup) {
+	return max($max, $fuelup->mileage->amount);
+}, 0);
 
 ?>
+<style>
+.fuelups-history {
+	width: 100%;
+	overflow: auto;
+	padding-bottom: 1px;
+	border-bottom: solid 2px black;
+	display: -webkit-box;
+	display: flex;
+}
+.fuelups-history .date {
+	align-self: center;
+	line-height: 70px;
+}
+.fuelups-history .date.start {
+	margin-right: 5px;
+}
+.fuelups-history .date.end {
+	margin-left: 5px;
+}
+.fuelups-history .bars {
+	height: 70px;
+	display: -webkit-flex;
+	display: -webkit-box;
+	display: flex;
+	-webkit-box-align: end;
+	align-items: flex-end;
+}
+.fuelups-history .bar {
+	width: 3px;
+	background-color: black;
+}
+.fuelups-history .bar + .bar {
+	margin-left: 1px;
+}
+</style>
+
 <h1><?= html($vehicle->name) ?></h1>
 
 <h2>Fuel up</h2>
@@ -25,7 +64,17 @@ $fuelups = $client->getFuelUpsWithIds($vehicle);
 	<p><button>Save</button></p>
 </form>
 
-<h2>Fuel-ups</h2>
+<h2>Fuel-ups (<?= count($fuelups) ?>)</h2>
+
+<div class="fuelups-history">
+	<div class="date start"><?= reset($fuelups)->date->format('Y M') ?></div>
+	<div class="bars">
+		<? foreach ($fuelups as $fuelup): ?>
+			<div class="bar" style="height: <?= number_format($fuelup->mileage->amount / $max * 100, 1) ?>%"></div>
+		<? endforeach ?>
+	</div>
+	<div class="date end"><?= end($fuelups)->date->format('Y M') ?></div>
+</div>
 
 <ul>
 	<? foreach ($fuelups as $fuelup): ?>
